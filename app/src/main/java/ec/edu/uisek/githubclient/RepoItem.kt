222917d73ec0a88aff4177726df4a5859ba17d20
matiaca25 <matias.romero@uisek.edu.ec>
2025-10-22 12:10:17 -0500

    laboratorio 3 Matias Romero

---
+++ app/src/main/java/ec/edu/uisek/githubclient/RepoItem.kt
@@ -0,0 +1,64 @@
+package ec.edu.uisek.githubclient
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ec.edu.uisek.githubclient.databinding.FragmentRepoItemBinding
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [Repoitem.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class RepoItem : Fragment() {
+    private var _binding: FragmentRepoItemBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentRepoItemBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.repoName.text = "Mi Repositorio"
+        binding.repoDescription.text = "Esta es la descripción del repositorio"
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment Repoitem.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            RepoItem().apply {
+                arguments = Bundle().apply {
+                }
+            }
+    }
+}
